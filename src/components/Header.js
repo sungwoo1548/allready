970d70ca88e7983ab75aaa142cbaa5b6e39971b1
sungwoo1548 <swo1548@gmail.com>
2019-11-30 04:07:57 +0900

    ing structuring

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <div>
+                <div>
+                    Header
+                </div>
+            </div>
+        </div>
+    )
+}
